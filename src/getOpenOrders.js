e34be9e15fbaf589c649fd14f978ffc5e33424ca
이찬호 <chanholee275@gmail.com>
2022-04-27 17:53:17 +0900

    feat: GET /api/orders를 위한 함수 작성

---
+++ src/getOpenOrders.js
@@ -0,0 +1,23 @@
+/* eslint-disable no-undef */
+import fetch from 'node-fetch';
+import process from 'process';
+
+import { streamToString } from './util.js';
+
+async function getOpenOrders(signature, time, market) {
+  const response = await fetch(process.env.API_URL + process.env.ORDERS_API + `?market=${market}`, {
+    method: 'GET',
+    headers: {
+      'FTX-KEY': process.env.ACCESS_KEY,
+      'FTX-SIGN': signature,
+      'FTX-TS': time,
+    },
+  });
+  const res = await streamToString(response.body);
+  console.log('조회 market : ' + market);
+  console.log('API 성공 여부 : ' + res.success);
+  console.log('API 결과 : ' + (res.success ? res.result : res.error));
+  console.log('API 응답 (JSON 형식) : ', res);
+}
+
+export default getOpenOrders;
